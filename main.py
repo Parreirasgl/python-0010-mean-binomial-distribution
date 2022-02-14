5d132825a38df452d84c585e7384ee203ead4078
Guilherme Parreiras <parreirasgl@hotmail.com>
2022-02-14 16:03:23 -0300

    Program working

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from random import randint
 
 num_suc = 0
 list_num_suc = []
+total_suc = 0
+num_simul = 10000
 
-for i in range(10):
+for i in range(num_simul):
 
-    for i in range(3):
+    for i2 in range(3):
         rand_num = randint(0,1)
         if rand_num == 1:
             num_suc += 1
@@ -13,4 +15,7 @@ for i in range(10):
     list_num_suc.append(num_suc)
     num_suc = 0
 
-print(list_num_suc)
+for i3 in list_num_suc:
+    total_suc = total_suc + i3
+
+print(total_suc / num_simul)
