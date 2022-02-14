e5b42afa0fff5e079c9c507a51c2c577fe493b5f
Guilherme Parreiras <parreirasgl@hotmail.com>
2022-02-14 15:52:07 -0300

    List of number of successes

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from random import randint
 
 num_suc = 0
+list_num_suc = []
 
-for i in range(3):
-    rand_num = randint(0,1)
-    if rand_num == 1:
-        num_suc += 1
+for i in range(10):
 
-print(num_suc)
+    for i in range(3):
+        rand_num = randint(0,1)
+        if rand_num == 1:
+            num_suc += 1
+
+    list_num_suc.append(num_suc)
+    num_suc = 0
+
+print(list_num_suc)
